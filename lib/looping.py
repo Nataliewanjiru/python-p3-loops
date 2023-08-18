5266809132e6a2d0919875c127c9c70ae7e8d748
Natalie Wanjiru <natalie.wanjiru@student.moringaschool.com>
2023-08-18 12:55:46 +0300

    Assignment Done

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i=10 
+    while i>=1:
+     if(i ==1):
+       print(i)
+       print("Happy New Year!")
+     else:
+       print(i)
+     i-=1
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    numbers = []
+    for num in int_list:
+        number = num ** 2
+        numbers.append(number)
+    return numbers  
+
+ 
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1  
+    while i <= 100:
+        if i % 3 == 0 and i % 5 != 0:
+            print("Fizz")
+        elif i % 5 == 0 and i % 3 != 0:
+            print("Buzz")
+        elif i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        else:
+            print(i)
+        i += 1
+
+
